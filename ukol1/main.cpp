25ab929f55bd63a521376742c6ca34c3ceb849d2
Ondrej Mekota <ondrej.mekota@me.com>
2017-11-03 12:16:36 +0100

    Initial Commit

---
+++ ukol1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ukol1
+//
+//  Created by Ondřej Měkota on 03/11/2017.
+//  Copyright © 2017 Ondřej Měkota. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
